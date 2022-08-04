13410b19a2e764cf8a2ff30af3f717e536f51e8a
Ihzakhofifah <107291614+IhzatulKhofifah@users.noreply.github.com>
2022-08-04 02:24:30 -0700

    Add files via upload

---
+++ dbconfig.php
@@ -0,0 +1,13 @@
+<?php 
+    /**
+     * 
+     *  Absensi Guru PAUD ABATASA
+     *  
+     */
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpwd = "";
+    $dbname = "absensi_guru";
+
+    $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+ ?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    /**
+     * 
+     *  Absensi Guru Paud ABATASA
+     *  Ig.me/fina_amn05
+     *  
+     */
+	echo "<script>window.alert;window.location=('../home');</script>"; 
+?>
